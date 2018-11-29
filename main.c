58a6d38646d092468caedd68f2597c23e6a9259b
dbwpdls22 <dbwpdls22@naver.com>
2018-11-29 15:31:23 +0900

    13_1

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	FILE* fp;
+	char name[100];
+	
+	printf("Input a file :");
+	scanf("%s",name);
+	
+	fp = fopen(name, "r");
+	
+	if(fp== NULL)
+	{
+		printf("error! check the file name:%s\n", name);
+		return -1 ;
+	}
+	
+	printf("%c\n",fgetc(fp));
+	
+	fclose(fp);
+	
+	return 0;
+}
